5a0ff5905a6f931ab9ab06fc1eab2bc7820886ba
brunodascouve <89090424+brunodascouve@users.noreply.github.com>
2022-05-07 22:45:36 -0300

    Add files via upload

---
+++ Teste 001.py
@@ -0,0 +1,16 @@
+introd = input('Qual é o seu nome?')
+print(introd,'É um prazer te conhecer!')
+print('Agora insira alguns dados para sua consulta...')
+
+
+dia = int{input('Dia = ')
+mes = int{input('Mes = ')
+ano = input('Ano = ')
+
+print('Você nasceu no dia',dia,'de',mes,'de',ano,'Correto?')
+
+print('Vamos trabalhar matemática agora...')
+num = input('Insira um número =')
+nume = input('Insira outro número =')
+s =(num + nume)
+print('A soma dos dois números é ...',s)
